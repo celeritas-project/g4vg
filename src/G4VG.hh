19a672dddd09e8b47b1b2a52763c2b6054a2fa58
Seth R Johnson <johnsonsr@ornl.gov>
2025-01-29 11:35:26 -0500

    Update headers

--- src/G4VG.hh
+++ src/G4VG.hh
@@ -1,6 +1,5 @@
-//----------------------------------*-C++-*----------------------------------//
-// Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
-// See the top-level COPYRIGHT file for details.
+//------------------------------- -*- C++ -*- -------------------------------//
+// Copyright G4VG contributors: see top-level COPYRIGHT file for details
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 //---------------------------------------------------------------------------//
 //! \file G4VG.hh
