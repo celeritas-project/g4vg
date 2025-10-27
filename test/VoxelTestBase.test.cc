a61d610a67ae6fa489cce43df35a9641c6c799b7
michele-colle <michelecolle108@gmail.com>
2025-10-27 17:54:50 +0100

    setup complete

---
+++ test/VoxelTestBase.test.cc
@@ -0,0 +1,85 @@
+//------------------------------- -*- C++ -*- -------------------------------//
+// Copyright G4VG contributors: see top-level COPYRIGHT file for details
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+//---------------------------------------------------------------------------//
+//! \file Custom.test.cc
+//---------------------------------------------------------------------------//
+
+#include <G4DisplacedSolid.hh>
+#include <G4LogicalVolume.hh>
+#include <G4Material.hh>
+#include <G4NistManager.hh>
+#include <G4Orb.hh>
+#include <G4PVPlacement.hh>
+#include <G4SolidStore.hh>
+#include <G4ThreeVector.hh>
+#include <gtest/gtest.h>
+
+#include "G4VG.hh"
+#include "TestBase.hh"
+
+namespace g4vg
+{
+namespace test
+{
+//---------------------------------------------------------------------------//
+using CustomTestBase = TestBase;
+
+//---------------------------------------------------------------------------//
+class VoxelTestBase : public CustomTestBase
+{
+  protected:
+    std::string basename() const final { return "voxel"; }
+    G4VPhysicalVolume* build_world() final;
+};
+
+G4VPhysicalVolume* VoxelTestBase::build_world()
+{
+    G4Material* mat = G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR");
+
+    auto* world_s = new G4Orb("world_solid", 100);
+    auto* world_l = new G4LogicalVolume(world_s, mat, "world");
+    auto* world_p = new G4PVPlacement(G4Transform3D{},
+                                      world_l,
+                                      "world_pv",
+                                      /* parent = */ nullptr,
+                                      /* many = */ false,
+                                      /* copy_no = */ 0);
+
+    return world_p;
+}
+TEST_F(VoxelTestBase, default_options)
+{
+    auto result = this->run(Options{});
+
+    TestResult ref;
+    ref.lv_name = {
+        "world",
+        "dright",
+        "dleft",
+    };
+    ref.solid_capacity = {
+        4188790.20478639,
+        4188.79020478639,
+        4199.11397533979,
+    };
+    ref.pv_name = {
+        "dright_pv",
+        "dleft_pv",
+        "world_pv",
+    };
+    ref.copy_no = {
+        0,
+        0,
+        0,
+    };
+    // Boolean volume calculation differs across platforms
+    EXPECT_TRUE(result.solid_capacity.size() >= 3
+                && result.solid_capacity[2] > 0);
+    ref.solid_capacity[2] = 0;
+    result.solid_capacity[2] = 0;
+    result.expect_eq(ref);
+}
+//---------------------------------------------------------------------------//
+}  // namespace test
+}  // namespace g4vg
